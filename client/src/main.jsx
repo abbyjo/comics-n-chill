a1d4c2e8689745a0d1ad87c0f7917e55831d0dfd
abbyjo <abbyjo225@gmail.com>
2024-03-10 15:21:35 -0400

    Added page routes to react dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+// Pages
 import App from './App.jsx'
-import './index.css'
+import Home from './pages/Home.jsx'
+import Submit from './pages/Submit.jsx'
+import Results from './pages/Results'
+import Admin from './pages/Admin.jsx'
+import Error from './pages/Error.jsx'
+
+// React router
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/submit',
+        element: <Submit />
+      }, {
+        path: '/results',
+        element: <Results />
+      }, {
+        path: '/admin',
+        element: <Admin />
+      }
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
